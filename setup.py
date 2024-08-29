8a64f7a5ccd8daf6defb92209c26ab7bb90cf50d
Prince Lakhani <princeal3855@gmail.com>
2024-08-29 23:34:49 +0530

    Updated folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '1.0.0',
+    author= 'Prince Lakhani',
+    author_email= 'princeal3855@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
